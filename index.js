4659e446eed57160de030efe967865ca9c74c317
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-06 17:29:56 +0200

    index

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import { like, dislike } from "./utils.js";
-
 import dogs from "./data.js";
 import Dog from "./Dogs.js";
 
-const firstDog = new Dog(dogs[1]);
+console.log(dogs);
+
+const firstDog = new Dog(dogs[0]);
 
 document.getElementById("dog-image").innerHTML = firstDog.renderNameBio();
-
-// const allDogs = ["Rex", "Bella", "Teddy"];
 
 // let hasBeenSwiped = false;
 // let hasBeenLiked = false;
