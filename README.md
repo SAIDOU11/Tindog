54f5fd9779a13a8528fa7da48f126e16922cfc3c
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-06 09:35:35 +0200

    new project tindog
    Solo Project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Tindog
- Tindog
+# The Frontend Developer Career Path
+
+# Solo Project Module 7 Scrimba Bootcamp
+
+- Tindog
---
+++ data.js
@@ -0,0 +1,26 @@
+const dogs = [
+  {
+    name: "Rex",
+    avatar: "images/dog-rex.jpg",
+    age: 25,
+    bio: "Art. Litterature. Natural wine. Yoga.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Rex",
+    avatar: "images/dog-rex.jpg",
+    age: 25,
+    bio: "Art. Litterature. Natural wine. Yoga.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Rex",
+    avatar: "images/dog-rex.jpg",
+    age: 25,
+    bio: "Art. Litterature. Natural wine. Yoga.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+];
