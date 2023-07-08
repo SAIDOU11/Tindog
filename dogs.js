3e08cc91bc1b72178a6d4cc93663daf063dfea1b
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-08 15:15:51 +0200

    tindog app

--- dogs.js
+++ dogs.js
@@ -1,10 +1,6 @@
-import { like, dislike } from "./utils.js";
-
 class Dog {
   constructor(data) {
     Object.assign(this, data);
-    this.hasBeenLiked = like(this.hasBeenLiked);
-    this.hasBeenSwiped = dislike(this.hasBeenSwiped);
   }
   renderNameBio() {
     const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this;
--- index.js
+++ index.js
@@ -1,8 +1,64 @@
 import dogsData from "./data.js";
 import Dog from "./dogs.js";
 
-const bella = new Dog(dogsData.Bella);
-const rex = new Dog(dogsData.Rex);
-const teddy = new Dog(dogsData.Teddy);
+let dogsArray = ["Teddy", "Rex", "Bella"];
 
-document.getElementById("dog-image").innerHTML = teddy.renderNameBio();
+let hasBeenLiked = false;
+let hasBeenSwiped = false;
+
+const getNewDog = () => {
+  const nextDogData = dogsData[dogsArray.shift()];
+  return nextDogData ? new Dog(nextDogData) : {};
+};
+
+const like = () => {
+  document.getElementById("like").addEventListener("click", () => {
+    console.log("button like");
+    hasBeenLiked = true;
+    document.querySelector(".stamp-like").classList.remove("hidden");
+    setTimeout(() => {
+      document.querySelector(".stamp-like").classList.add("hidden");
+      if (hasBeenLiked) {
+        return (document.body.innerHTML = ` 
+    <div class="find-love">
+      <h2>Congratulations <br> your find <br> your love</h2> 
+      <h2>❤️</h2>
+    </div>  
+    `);
+      }
+    }, 3000);
+  });
+};
+like();
+
+const dislike = () => {
+  document.getElementById("dislike").addEventListener("click", () => {
+    console.log("button dislike");
+    hasBeenSwiped = true;
+    document.querySelector(".stamp-none").classList.remove("hidden");
+
+    setTimeout(() => {
+      document.querySelector(".stamp-none").classList.add("hidden");
+      if (hasBeenSwiped) {
+        if (dogsArray.length > 0) {
+          dogProfil = getNewDog();
+          render();
+        } else {
+          return (document.body.innerHTML = ` 
+            <div class="no-love">
+              <h2>We're sorry <br> there is <br> no love for you</h2> 
+              <h2>💔</h2>
+            </div>  
+            `);
+        }
+      }
+    }, 2000);
+  });
+};
+dislike();
+let dogProfil = getNewDog();
+
+function render() {
+  document.getElementById("dog-image").innerHTML = dogProfil.renderNameBio();
+}
+render();
